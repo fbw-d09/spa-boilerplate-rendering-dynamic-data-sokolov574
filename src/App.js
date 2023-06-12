40695b2e0a5941d875609975277280a69dec2a06
sokolov574 <sokolovloveoleg@gmail.com>
2023-06-12 14:28:42 +0200

    Add rendering on the page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 const customer = {
   first_name: 'Bob',
   last_name: 'Dylan',
@@ -5,7 +6,12 @@ const customer = {
 
 function App() {
   //your code inside these <div>
-  return <div></div>;
+  return (
+    <div>
+      <h1>My name is {customer.first_name}</h1>
+      <h2>My last name is {customer.last_name}</h2>
+    </div>
+  );
 }
 
 export default App;
